41dc051bb0496de61b6498fccbdda68f129be182
donald <you@example.com>
2025-11-18 14:46:48 +0545

    Todo Component break down

---
+++ src/components/AddTodo.jsx
@@ -0,0 +1,20 @@
+function AddTodo() {
+  return (
+    <div class="container ">
+      <div class="row space-item ">
+        <div class="col-5">
+          <input type="text" placeholder="Enter your word" />
+        </div>
+        <div class="col-4">
+          <input type="date" />
+        </div>
+        <div class="col-3">
+          <button type="button" class="btn btn-info">
+            Info
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default AddTodo;
---
+++ src/components/TodoItem1.jsx
@@ -0,0 +1,18 @@
+function TodoItem1() {
+  let todoname = "purchase snacks";
+  let tododate = "6/2/2025";
+  return (
+    <div class="container ">
+      <div class="row space-item">
+        <div class="col-5">{todoname}</div>
+        <div class="col-4">{tododate}</div>
+        <div class="col-3">
+          <button type="button" class="btn btn-danger">
+            Danger
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default TodoItem1;
